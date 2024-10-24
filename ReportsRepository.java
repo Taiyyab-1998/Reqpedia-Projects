828ee8883047ac1199ca7d3e8d8b2f8f457f43d0
Taiyyab-1998 <104361606+Taiyyab-1998@users.noreply.github.com>
2024-10-24 21:03:07 +0530

    Delete ReportsRepository.java

--- ReportsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Jdbc_hibernate.spring_jdbc.jobs;
-
-import com.Jdbc_hibernate.spring_jdbc.jobs.Reports;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportsRepository extends JpaRepository<Reports, Long> {
-}
